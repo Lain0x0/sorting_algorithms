754e376116c85b1dd6fe08bf263598a4ea0b93d1
Lain0x0 <lainyamato03@gmail.com>
2024-03-05 19:54:23 +0100

    bubble sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,50 +3,53 @@
 /**
  * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
  * @h: A pointer to the head of the doubly-linked list.
- * @n1st: A pointer to the first node to swap.
- * @n2nd: The second node to swap.
+ * @na: A pointer to the first node to swap.
+ * @nb: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1st, listint_t *n2nd)
+void swap_nodes(listint_t **h, listint_t **na, listint_t *nb)
 {
-	(*n1st)->next = n2nd->next;
-	if (n2nd->next != NULL)
-		n2nd->next->prev = *n1st;
-
-	n2nd->prev = (*n1st)->prev;
-	n2nd->next = *n1st;
-
-	if ((*n1st)->prev != NULL)
-		(*n1st)->prev->next = n2nd;
+	(*na)->next = nb->next;
+	if (nb->next != NULL)
+		nb->next->prev = *na;
+	nb->prev = (*na)->prev;
+	nb->next = *na;
+	if ((*na)->prev != NULL)
+		(*na)->prev->next = nb;
 	else
-		*h = n2nd;
-
-	(*n1st)->prev = n2nd;
-	*n1st = n2nd->prev;
+		*h = nb;
+	(*na)->prev = nb;
+	*na = nb->prev;
 }
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * using the insertion sort algorithm.
+ *                       using the insertion sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-
-	listint_t *iter, *tmp;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	int i, key, j;
+	for (i = 1; i < n; i++) 
 	{
-		tmp = iter->next;
+		key = arr[i];
+		j = i - 1;
 
-		while (iter->prev != NULL && iter->n < iter->prev->n)
+		while (j >= 0 && arr[j] > key) 
 		{
-			swap_nodes(list, &(iter->prev), iter);
-			print_list((const listint_t *)*list);
+			arr[j + 1] = arr[j];
+			j = j - 1;
 		}
+		arr[j + 1] = key;
+	}
+	{
+		swap_nodes(list, &insert, iter);
+		print_list((const listint_t *)*list);
 	}
 }
