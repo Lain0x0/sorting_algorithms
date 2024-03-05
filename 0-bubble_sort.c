3b692b29055a4c054128ff96580a1ccadc8ac967
Lain0x0 <lainyamato03@gmail.com>
2024-03-05 10:04:09 +0100

    bubble_sorting.exe

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,7 +34,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap_ints(array + j, array + j + 1);
+				swap_int(array + j, array + j + 1);
 				print_array(array, size);
 				bubble = false;
 			}
